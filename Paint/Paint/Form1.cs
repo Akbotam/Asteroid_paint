f8ddd647cbe1b2667c686005cbd54c7665bd21dc
Akbota <bmautkazy@gmail.com>
2017-04-13 07:28:36 +0500

    comment

---
+++ Paint/Paint/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace Paint
+{
+    public partial class Form1 : Form
+    {
+        public PaintBase paintBase;
+        public Form1()
+        {
+            InitializeComponent();
+            paintBase = new PaintBase(pictureBox1);
+        }  
+
+ 
+
+        private void SetPenColor_Click(object sender, EventArgs e)
+        {
+            Color color = ((Button)sender).BackColor;
+            paintBase.setPenColor(color);
+        }
+
+        private void picturebox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            paintBase.mouseClicked = true;
+            paintBase.prev = e.Location;
+        }
+
+        private void picturebox1_MouseMove(object sender, MouseEventArgs e)
+        {    if (paintBase.mouseClicked)
+            {
+                paintBase.Draw(pictureBox1.CreateGraphics(), e.Location);
+            }
+
+        }
+
+        private void picturebox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            paintBase.mouseClicked = false;
+            
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            paintBase.pen.Width = trackBar1.Value;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+           
+            paintBase.shape = PaintBase.Shape.RECTANGLE;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            paintBase.shape = PaintBase.Shape.PEN; 
+        }
+    }
+}
---
+++ Paint/Paint/PaintBase.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+
+namespace Paint
+{
+
+    
+    public class PaintBase
+    {
+        public enum Shape { LINE, ERASER, PEN, RECTANGLE, ELLIPSE };
+
+        public Graphics g;
+        public Pen pen;
+        public Bitmap btm;
+        public Point prev;
+        public Shape shape;
+        public PictureBox picture;
+        public bool mouseClicked;
+
+        public PaintBase (PictureBox picturebox1)
+        {
+            btm = new Bitmap(picture.Width, picture.Height);
+
+            bool mouseClicked = false;
+            picture = picturebox1;
+            pen = new Pen(Color.Black);
+            prev = new Point(0, 0);
+            //g.Clear(Color.White);
+            shape = Shape.PEN;
+        }
+        public void Draw (Graphics g, Point curPoint)
+        {
+            if (shape ==Shape.PEN)
+            {
+                g.DrawLine(pen, prev, curPoint);
+                prev = curPoint;
+
+            }
+            else if (shape == Shape.RECTANGLE)
+            {
+                int w = Math.Abs(prev.X - curPoint.X);
+                int h = Math.Abs(prev.Y - curPoint.Y);
+                int MinX = Math.Min(prev.X, curPoint.X);
+                int MinY = Math.Min(prev.Y, curPoint.Y);
+
+                g.DrawRectangle(pen, MinX, MinY, w, h);
+            }
+        }
+
+        public void setPenColor(Color color)
+        {
+            pen.Color = color;
+        }
+    }
+
+}
