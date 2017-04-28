e675a2f90f0a034ff90ed2b0fe3070d91e86fb2e
Akbota <bmautkazy@gmail.com>
2017-04-29 03:19:05 +0500

    comment

--- Asteroids/Asteroids/Form1.Designer.cs
+++ Asteroids/Asteroids/Form1.Designer.cs
@@ -28,19 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(1, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(943, 547);
+            this.pictureBox1.Size = new System.Drawing.Size(991, 571);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
@@ -52,6 +59,7 @@
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Paint);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
@@ -60,6 +68,7 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Asteroids/Asteroids/Form1.cs
+++ Asteroids/Asteroids/Form1.cs
@@ -13,16 +13,28 @@ namespace Asteroids
 {
     public partial class Form1 : Form
     {
+        enum Direction
+        {
+            UP,
+            DOWN,
+            RIGHT,
+            LEFT,
+            NONE
+        };
+        Direction dir;
+
         Bitmap bmp;
         Graphics g;
         Ship s;
         Bullet b;
         Triangle shape1, shape2, shape3, shape4;
-        //Graphics gpic;
+        public static bool BulIsEx = false;
+        public static int bul = 0;
         SolidBrush brush1;
         SolidBrush brush2;
         SolidBrush brush3;
         SolidBrush brush4;
+        public int x = 400, y = 170;
 
         public Form1()
         {
@@ -35,7 +47,7 @@ namespace Asteroids
             brush2 = new SolidBrush(Color.Red);
             brush3 = new SolidBrush(Color.Green);
             brush4 = new SolidBrush(Color.Yellow);
-            s = new Ship(400, 170);
+            s = new Ship(x, y);
             b = new Bullet(420, 140);
             shape1 = new Triangle(0, -20);
             shape2 = new Triangle(90, 150);
@@ -58,22 +70,116 @@ namespace Asteroids
 
         }
 
-        private void Form1_Paint(object sender, EventArgs e)
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Enter) // если Enter, то происходит выстрел
+            {
+                BulIsEx = true; // пуля существует
+                Refresh();
+                pictureBox1.Image = bmp;
+            }
+
+            if (e.KeyCode == Keys.Up)
+            {
+                dir = Direction.UP;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                dir = Direction.DOWN;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                dir = Direction.RIGHT;
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                dir = Direction.LEFT;
+            }
+        }
+
+        static int x1 = 0, x2 = 90, x3 = 500, x4 = 600;
+        static int y1 = -20, y2 = 150, y3 = 180, y4 = -40;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            bmp = new Bitmap(@"C:\Users\Андрей\Desktop\Professional English (B2)/bitmapfon.jpg");
+            g = Graphics.FromImage(bmp);
+
+            pictureBox1.Image = bmp;
+
+            x1++; // asteroids
+            y1++;
+            if (x1 == 991)
+                x1 = 0;
+            if (y1 == 571)
+                y1 = 0;
+            x2++;
+            y2--;
+            if (x2 == 991)
+                x2 = 0;
+            if (y2 == 0)
+                y2 = 571;
+            x3--;
+            y3++;
+            if (x3 == 0)
+                x3 = 991;
+            if (y3 == 571)
+                y3 = 0;
+            x4--;
+            y4--;
+            if (x4 == 0)
+                x4 = 991;
+            if (y4 == 0)
+                y4 = 571;
+            shape1 = new Triangle(x1, y1);
+            shape2 = new Triangle(x2, y2);
+            shape3 = new Triangle(x3, y3);
+            shape4 = new Triangle(x4, y4);
+            if (BulIsEx) // bullet
+            {
+                bul += 4;
+                b = new Bullet(280, 120 - bul);
+            }
+            if (dir == Direction.UP)
+            {
+                s = new Ship(x, y -= 10);
+            }
+            if (dir == Direction.DOWN)
+            {
+                s = new Ship(x, y += 10);
+            }
+            if (dir == Direction.RIGHT)
+            {
+                s = new Ship(x += 10, y);
+            }
+            if (dir == Direction.LEFT)
+            {
+                s = new Ship(x -= 10, y);
+            }
+
+            Paint1();
+            pictureBox1.Image = bmp;
+            pictureBox1.Refresh();
+        }
+
+        
+
+        public void Paint1()
         {
             g.FillPath(brush2, shape1.path);
-            
+
             g.FillPath(brush2, shape1.path2);
-            
+
             g.FillPath(brush2, shape2.path);
-           
+
             g.FillPath(brush2, shape2.path2);
-            
+
             g.FillPath(brush2, shape3.path);
-            
+
             g.FillPath(brush2, shape3.path2);
-           
+
             g.FillPath(brush2, shape4.path);
-           
+
             g.FillPath(brush2, shape4.path2);
 
 
@@ -91,8 +197,51 @@ namespace Asteroids
             g.DrawPath(new Pen(Color.Green), s.path4); // gun
             g.FillPath(brush3, s.path4);
 
-            g.DrawPath(new Pen(Color.Green), b.path1); //bullet
-            g.FillPath(brush3, b.path1);
+            if (BulIsEx)
+            {
+                g.DrawPath(new Pen(Color.Green), b.path1); //bullet
+                g.FillPath(brush3, b.path1);
+            }
+        }
+        private void Form1_Paint(object sender, EventArgs e)
+        {
+
+            //g.FillPath(brush2, shape1.path);
+
+            //g.FillPath(brush2, shape1.path2);
+
+            //g.FillPath(brush2, shape2.path);
+
+            //g.FillPath(brush2, shape2.path2);
+
+            //g.FillPath(brush2, shape3.path);
+
+            //g.FillPath(brush2, shape3.path2);
+
+            //g.FillPath(brush2, shape4.path);
+
+            //g.FillPath(brush2, shape4.path2);
+
+
+            //g.FillEllipse(brush1, 70, 100, 35, 35);
+            //g.FillEllipse(brush1, 100, 450, 35, 35);
+            //g.FillEllipse(brush1, 350, 60, 35, 35);
+            //g.FillEllipse(brush1, 600, 90, 35, 35);
+            //g.FillEllipse(brush1, 940, 150, 35, 35);
+            //g.FillEllipse(brush1, 800, 250, 35, 35);
+            //g.FillEllipse(brush1, 920, 500, 35, 35);
+            //g.FillEllipse(brush1, 360, 400, 35, 35);
+
+            //g.DrawPath(new Pen(Color.Yellow), s.path3); // ship
+            //g.FillPath(brush4, s.path3);
+            //g.DrawPath(new Pen(Color.Green), s.path4); // gun
+            //g.FillPath(brush3, s.path4);
+
+            //if (BulIsEx)
+            //{
+            //    g.DrawPath(new Pen(Color.Green), b.path1); //bullet
+            //    g.FillPath(brush3, b.path1);
+            //}
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
